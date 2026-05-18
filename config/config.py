6e05831ac0ccbc749ffdc3f6027635bb708c269c
Anton Stadolnik <stadolnikanton1@gmail.com>
2026-05-18 23:53:11 +0300

    Исправленна опечатка

--- config/config.py
+++ config/config.py
@@ -8,4 +8,4 @@ TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_CHAT_ID_ENV = os.getenv("ADMIN_CHAT_ID")
 ADMIN_CHAT_ID = int(ADMIN_CHAT_ID_ENV) if ADMIN_CHAT_ID_ENV else None
 
-HEALTH_URL = os.getenv("HEALT_URL")
+HEALTH_URL = os.getenv("HEALTH_URL")
